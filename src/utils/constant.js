91f03fef8ef3ddc0dadeb4cf71e7af8c7f8432bd
Anurag Dangi <anuragdangi015@gmail.com>
2024-02-16 19:54:51 +0530

    few changes for the deployment

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = 'AIzaSyBjGAkuBJniwnIHqB6AnFm4wxv4DQpCVBc';
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const OFFSET_LIVE_CHAT = 20;
 
